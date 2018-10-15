c6d9dd4ac7e616405ef0465b079a4a95f7289059
tomoscase <tomoscase@gmail.com>
2018-10-15 01:31:23 -0700

    create a server

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const debug = require('debug')('app');
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, '/src/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index', {title: 'Index'});
+});
+
+app.listen(3000, function(){
+  console.log('PS Project Running on port 3000!')
+});
