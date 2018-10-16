2c8280868277decf3924fc5e51d295146deb0872
tomoscase <tomoscase@gmail.com>
2018-10-16 01:32:33 -0700

    module2

--- src/app.js
+++ src/app.js
@@ -6,13 +6,38 @@ const express = require('express');
 
 const app = express();
 
+// set location for static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set view directory and view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json','accounts.json'),'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'),'utf8');
+const users = JSON.parse(userData);
+
+app.get('/savings', function(req, res){
+  res.render('account', {account: accounts.savings});
+})
+app.get('/checking', function(req, res){
+  res.render('account', {account: accounts.checking});
+})
+app.get('/credit', function(req, res){
+  res.render('account', {account: accounts.credit});
+})
+app.get('/profile', function(req, res){
+  res.render('profile', {user:users[0]});
+})
+
 app.get('/', function(req, res){
-  res.render('index', {title: 'Index'});
+  res.render('index', {
+    //title: 'Index'
+    title: 'Account Summary',
+    accounts: accounts
+  });
 });
 
 app.listen(3000, function(){
